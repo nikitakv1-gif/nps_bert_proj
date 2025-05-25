518596a40e4ffd7ba49304237f1d8a28abf2d77a
nizenkiy <66327241+nikitakv1-gif@users.noreply.github.com>
2025-05-25 20:19:14 +0300

    Update model.py

--- model.py
+++ model.py
@@ -9,5 +9,8 @@ app.config['tokenizer'] = tokenizer
 app.config["model_sent"] = model_sent
 app.config['emotion_tokenizer'] = emotion_tokenizer
 
+application = app
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ,get("PORT", 4000))
+    app.run(host = "0.0.0.0", port = port)
